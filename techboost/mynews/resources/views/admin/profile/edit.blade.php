e5fd1f5f1eb0258d7fbeda9a982cfd07ad7fe338
am33119 <so.20181201@gmail.com>
2019-01-10 11:55:50 +0900

    lesson14

--- techboost/mynews/resources/views/admin/profile/edit.blade.php
+++ techboost/mynews/resources/views/admin/profile/edit.blade.php
@@ -1,3 +1,62 @@
+@extends('layouts.profile')
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">タイトル</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">性別</label>
+                        <div class="col-md-10">
+                            <ul>
+                              <li>男性<input type="radio" class="form-control" name="gender" value="男"></li>
+                              <li>女性<input type="radio" class="form-control" name="gender" value="女"></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="hobby" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,9 +64,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>練習</title>
+        <title>プロフィール</title>
     </head>
     <body>
-        <h1>練習画面</h1>
+        <h1>プロフィールのページ</h1>
     </body>
 </html>
--- techboost/mynews/routes/web.php
+++ techboost/mynews/routes/web.php
@@ -15,9 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 
 
