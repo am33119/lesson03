f9d308cb48d8986147598940c61b327c4b0b80ee
am33119 <so.20181201@gmail.com>
2019-01-13 11:33:43 +0900

    lesson14-3 修正

--- techboost/mynews/routes/web.php
+++ techboost/mynews/routes/web.php
@@ -16,14 +16,14 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
-    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
-    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
-     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update'); 
+     Route::get('news/delete', 'Admin\NewsController@delete');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 
